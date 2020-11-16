17d8dd6aedf512ae06b3a276c14ac85d70f8cdfa
Colin <colintklein@gmail.com>
2020-11-16 15:17:33 -0600

    Writes some practice code

--- CSharpPractice/Program.cs
+++ CSharpPractice/Program.cs
@@ -6,7 +6,35 @@ namespace CSharpPractice
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            /* use this space to write your own short program! 
+            Here's what you learned:
+
+            DATA TYPES: int, double, char, string, bool
+            VARIABLES: datatype variableName = value;
+            COMMON ERRORS: wrong type, wrong value, no semicolon
+            DATA TYPE CONVERSION: implicit, explicit, methods
+
+            Good luck! */
+
+            /*
+             * Now that you know a few things about variables and data types, try writing a program that:
+             * Converts a boolean to a string
+             * Changes a string to a list of chars
+             * Converts a data type we didn’t cover to another data type!
+             */
+
+            bool isVerified = true;
+
+            // Convert isVerified from bool to string and print
+            string converted = Convert.ToString(isVerified);
+            Console.WriteLine(converted);
+
+            // Change string to list of chars
+            for (int i = 0; i < converted.Length; i++)
+            {
+                Console.WriteLine(Convert.ToChar(converted[i]));
+            }
+
         }
     }
 }
